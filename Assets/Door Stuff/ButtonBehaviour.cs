3c7e78833a73efb519b60f259bce8c72ae784ab6
NaoEdoJose <naoedojose.nave@gmail.com>
2015-09-17 14:51:29 -0300

    Typing Text Stuff
    The start of text stuff and buttons/doors behaviours

---
+++ Assets/Door Stuff/ButtonBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonBehaviour : MonoBehaviour {
+
+	public int id ;
+
+	public bool pressed;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/TextManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TextManager : MonoBehaviour {
+
+	public string phrase;
+
+	char[] array ;
+
+	char letter;
+
+	private TextMesh txt;
+
+	// Use this for initialization
+	void Start () {
+		txt = GetComponentInChildren<TextMesh> ();
+		array = phrase.ToCharArray ();
+		StartCoroutine(writer(0.1f));
+	}
+
+	IEnumerator writer(float f){
+
+		for (int i = 0; i < array.Length; i++) {
+			
+			letter = array[i];
+			Debug.Log (letter);
+			txt.text += letter;
+			yield return new WaitForSeconds (f);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+
+
+	}
+}
