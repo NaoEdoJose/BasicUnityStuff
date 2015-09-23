9c4b604c3478ae23c6e70a372c9666e5a787caf6
NaoEdoJose <naoedojose.nave@gmail.com>
2015-09-23 13:42:53 -0300

    Revert "Revert "Color Shuffle and Dinamic Camera""
    This reverts commit 5752f744fcca87fbddd15d14c2a5c180d422a1cc.

---
+++ Assets/DinamicCamera/DinamicCamera.cs
@@ -0,0 +1,22 @@
+﻿//By ThiagoSemJoelho
+using UnityEngine;
+using System.Collections;
+
+public class DinamicCamera : MonoBehaviour {
+
+	public GameObject target;
+	public float startX;
+	public float endX;
+
+	void Update () {
+		float targetX = target.transform.position.x;
+
+		if (targetX < startX)
+			targetX = startX;
+		else if (targetX > endX)
+			targetX = endX;
+
+		Vector3 targetPosition = new Vector3 (targetX, 0, -10); 
+		gameObject.transform.position = targetPosition;
+	}
+}
